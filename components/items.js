dcc59471101b29f4d9d87113ef2e298b0ac39a84
naihloan <benjaminjuarezarlt@gmail.com>
2021-06-08 22:56:15 -0300

    update for cta and header and footer components blog 2021

--- components/items.js
+++ components/items.js
@@ -7,17 +7,35 @@ class Items extends HTMLElement{
             <div class="row">
 
               <div class="col-md-4">
-                <h2>Project Title</h2>
-                <p>Lipsum hello</p>
-                <p><a class="btn btn-secondary" href="#" role="button">Ver detalles &darr;
+                <h2>Bio</h2>
+                <p>Training UX</p>
+                <p><a class="btn btn-secondary" href="#" role="button">cv &darr;
                 <span class="glyphicon glyphicon-arrow-down"></span>
                 </a></p>
               </div>
 
               <div class="col-md-4">
-                <h2>Project Title</h2>
-                <p>Lipsum hello</p>
-                <p><a class="btn btn-secondary" href="#" role="button">Ver detalles &darr;
+                <h2>Vermicomposting</h2>
+                <p>how to use your green garbage to feed an earthworm colony</p>
+                <p><a class="btn btn-secondary" href="#" role="button">worms freezer cheatsheet &darr;
+                <span class="glyphicon glyphicon-arrow-down"></span>
+                </a></p>
+              </div>
+              </div>
+              
+              <div class="row">
+              <div class="col-md-4">
+                <h2>Ultra Running</h2>
+                <p>how and why to go <del>crazy</del> long </p>
+                <p><a class="btn btn-secondary" href="#" role="button">PhD Project &darr;
+                <span class="glyphicon glyphicon-arrow-down"></span>
+                </a></p>
+              </div>
+              
+              <div class="col-md-4">
+                <h2>Graffiti and Pixação</h2>
+                <p>Urban art in Brazil</p>
+                <p><a class="btn btn-secondary" href="#" role="button">Master Thesis &darr;
                 <span class="glyphicon glyphicon-arrow-down"></span>
                 </a></p>
               </div>
--- components/navbar.js
+++ components/navbar.js
@@ -3,7 +3,7 @@ class Navbar extends HTMLElement{
         this.id = id;
         this.innerHTML = `
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-          <a class="navbar-brand" href="#">Launcher</a>
+          <a class="navbar-brand" href="#">ramoneando.com</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
           </button>
